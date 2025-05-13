641c5b7671da80b5281dcf2843c2b56abe82b35b
Jeff Zeng <72146519+zhh135@users.noreply.github.com>
2025-05-13 16:44:49 +0800

    [Feat] 完成数据模型的构建。

---
+++ Authy/Model/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace Authy.Models
+{
+    public struct Account
+    {
+        public string Name { get; set; }
+        public string Resource { get; set; }
+        public string Key { get; set; }
+    }
+}
---
+++ Authy/Model/VaultItem.cs
@@ -0,0 +1,14 @@
+﻿namespace Authy.Models
+{
+    public class VaultItem
+    {
+        public string Name { get; set; }
+        public string Resource { get; set; }
+
+        public VaultItem(string name, string resource)
+        {
+            Name = name;
+            Resource = resource;
+        }
+    }
+}
